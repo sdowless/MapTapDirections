a184ad41891e24c7cf24e868600399f91d17a79d
Stephan Dowless <dowless.stephan@gmail.com>
2022-04-18 11:23:14 -0400

    Initial Commit

---
+++ MapTapDirections/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapTapDirections
+//
+//  Created by Stephen Dowless on 4/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapTapDirections/MapTapDirectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTapDirectionsApp.swift
+//  MapTapDirections
+//
+//  Created by Stephen Dowless on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MapTapDirectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
